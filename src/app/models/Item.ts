b9cf8b7a3515af2ae763873c18f0dffe4019e78b
David Villaron <david.villaron@outlook.com>
2023-01-12 23:55:22 -0600

    added models/interface

---
+++ src/app/models/Item.ts
@@ -0,0 +1,10 @@
+
+export interface Item {
+    id: number | null;
+    name: string;
+    image: string;
+    weight: number;
+    quantity: number;
+    calories: number;
+    category: string;
+}
