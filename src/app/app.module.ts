ff0700cafcb83d2abb065dc7d7f8547778d4f39e
David Villaron <david.villaron@outlook.com>
2023-01-12 23:11:42 -0600

    inital setup components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { HomeComponent } from './components/home/home.component';
+import { PantryComponent } from './components/pantry/pantry.component';
+import { ItemAddComponent } from './components/item-add/item-add.component';
+import { ItemViewComponent } from './components/item-view/item-view.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
+import { RecipeAddComponent } from './components/recipe-add/recipe-add.component';
+import { RecipeViewComponent } from './components/recipe-view/recipe-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent,
+    PantryComponent,
+    ItemAddComponent,
+    ItemViewComponent,
+    RecipeComponent,
+    RecipeAddComponent,
+    RecipeViewComponent
   ],
   imports: [
     BrowserModule
