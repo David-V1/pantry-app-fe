628cc9696c82b055a4447ef3e13d5c8b145f1506
David Villaron <david.villaron@outlook.com>
2023-01-15 18:22:00 -0600

    Added 3 hero Recipes to HOME component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 import { PageName } from 'src/app/enums/PageEnum';
+import { RecipeService } from 'src/app/services/recipe.service';
+import { Recipe } from 'src/app/models/Recipe';
+import { map, pipe, take, tap } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,8 +12,18 @@ import { PageName } from 'src/app/enums/PageEnum';
 })
 export class HomeComponent {
   public pageName = PageName;
+  public heroRecipes: Recipe[] =[]
 
-  constructor(public ui: UiService) { }
+  constructor(public ui: UiService, public recipeService: RecipeService) {
+    this.recipeService.recipes$
+    .pipe(map(recipes =>recipes.filter(obj => obj.id! <= 3))) // first 3 recipes
+    .subscribe({
+      next: recipes => this.heroRecipes = recipes,
+      error: err => console.log(err)
+    })
+    this.recipeService.getRecipes();
+   }
+   
 
   public onViewRecipes(): void {
     this.ui.changePage(PageName.RECIPE);
